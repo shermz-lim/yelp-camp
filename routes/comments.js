15fa3037dc5363cba4424b3df001df92407b335b
Sherman Lim <shermz.lim.jh@gmail.com>
2019-01-06 10:54:50 +0000

    Added campground.rating function, & updated comments routes to update campground.rating

--- routes/comments.js
+++ routes/comments.js
@@ -25,7 +25,7 @@ router.get("/new", middleware.isLoggedIn, middleware.checkUserReviewed, function
 
 // CREATE
 router.post("/", middleware.isLoggedIn, middleware.checkUserReviewed, function(req, res){
-    Campground.findById(req.params.id, function(err, foundCampground){
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
         if (!foundCampground || err) {
             console.log(err);
             res.redirect("/campgrounds");
@@ -40,6 +40,7 @@ router.post("/", middleware.isLoggedIn, middleware.checkUserReviewed, function(r
                     newComment.author.username = req.user.username;
                     newComment.save();
                     foundCampground.comments.push(newComment);
+                    foundCampground.rating = calculateAverage(foundCampground.comments)
                     foundCampground.save(function(err){
                         if (err) {
                             console.log(err);
@@ -85,8 +86,18 @@ router.put("/:comment_id", middleware.checkCommentOwnership, function(req, res){
             req.flash('error', 'Fail to create comment.')
             res.redirect("back")
         } else {
-            req.flash("success", "Comment updated.")
-            res.redirect("/campgrounds/" + req.params.id);
+            Campground.findById(req.params.id).populate("comments").exec(function(err, campground){
+                if (!campground || err) {
+                    console.log(err);
+                    req.flash("error", "Fail to update campground's rating.")
+                    res.redirect("/campgrounds")
+                } else {
+                    campground.rating = calculateAverage(campground.comments)
+                    campground.save()
+                    req.flash("success", "Comment updated.")
+                    res.redirect("/campgrounds/" + req.params.id);
+                }
+            })
         };
     });
 });
@@ -99,13 +110,34 @@ router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, re
             req.flash('error', 'Fail to delete comment.')
             res.redirect("back")
         } else {
-            req.flash("success", "Comment deleted successfully.")
-            res.redirect("/campgrounds/" + req.params.id);
+            Campground.findByIdAndUpdate(req.params.id, {$pull: {comments: req.params.comment_id}}, {new: true}).populate("comments").exec(function (err, campground) {
+                if (err) {
+                    console.log(err);
+                    req.flash("error", "An error occured.");
+                    res.redirect("back");
+                } else {
+                    campground.rating = calculateAverage(campground.comments)
+                    campground.save()
+                    req.flash("success", "Comment deleted successfully.")
+                    res.redirect("/campgrounds/" + req.params.id);
+                };
+            });
         };
     });
 });
 
 
+// function for calculating campground's average rating 
+function calculateAverage(comments) {
+    if (comments.length === 0) {
+        return 0;
+    }
+    var sum = 0;
+    comments.forEach(function (element) {
+        sum += element.rating;
+    });
+    return sum / comments.length;
+}
 
 
 module.exports = router;
